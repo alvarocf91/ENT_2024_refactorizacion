c308f0f627f8c5caf82885a2bb6a30d7f597e7ae
Mañana <Maana@Aula8-09>
2024-04-16 10:15:02 +0200

    Creamos una Calculadora de pedido

--- src/ejercicio2/Tienda.java
+++ src/ejercicio2/Tienda.java
@@ -1,5 +1,23 @@
 package ejercicio2;
 
 public class Tienda {
+	
+    private List<Producto> productos;
+    private List<Cliente> clientes;
+    private List<Pedido> pedidos;
 
+   
+  
+
+    public void agregarCliente(Cliente cliente) {
+        // Lógica para agregar un cliente
+    }
+
+    public void eliminarCliente(Cliente cliente) {
+        // Lógica para eliminar un cliente
+    }
+
+
+    // Otros métodos relacionados con la gestión de la tienda
 }
+
